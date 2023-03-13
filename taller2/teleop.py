c4921bcafdf5ce5f337915ec817633de141a547b
Tony Montes <t.montes@uniandes.edu.co>
2023-03-13 18:17:47 -0500

    add joystick changes

--- taller2/teleop.py
+++ taller2/teleop.py
@@ -14,15 +14,55 @@ class TurtleBotTeleop(Node):
     def __init__(self):
         super().__init__('turtle_bot_teleop')
         self.publisher_ = self.create_publisher(Twist, 'turtlebot_cmdVel', 20)
-        joystick = pygame.joystick.get_count()
-        joystick.init()
+        self.joystick = pygame.joystick.get_count()
+        self.joystick.init()
+        self.timer = self.create_timer(0.1, self.timer_callback)
+
+        joystick_count = pygame.joystick.get_count()
+        if joystick_count == 0:
+            print("No se detectaron dispositivos de joystick.")
+            pygame.quit()
+            exit()
+        else:
+            print("Hay" + str(joystick_count))
+
         #listener = keyboard.Listener(on_press=self.on_presss, on_release=self.on_release)
         #listener.start()
+
         self.linear = float(input("Ingrese la velocidad lineal: "))
         self.angular = float(input("Ingrese la velocidad angular: "))
         self.prevchar = ''
 
+    def timer_callback(self):
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                pygame.quit()
+                quit()
+        
+        x_axis_left = self.joystick.get_axis(0)
+        y_axis_right = self.joystick.get_axis(3)
+        triggerR = self.joystick.get_axis(4)
+        triggerL = self.joystick.get_axis(5)
+
+        minVal = 0.1 # si el valor leído es menor a 0.1, no lo lee
+
+        mov = []
+        if (abs(x_axis_left) > minVal):
+            mov.append("Izquierda" if x_axis_left < 0 else "Derecha")
+        if (abs(y_axis_right) > minVal):
+            mov.append("Abajo" if y_axis_right > 0 else "Arriba")
+        if (triggerL > -1):
+            mov.append("TriggerL")
+        if (triggerR > -1):
+            mov.append("TriggerR")
+
+        if len(mov) > 0:
+            print(mov)
+
+
     def on_presss(self, key):
+
+
 
         if not(hasattr(key, 'char')): 
             self.get_logger().info('Invalid input')
