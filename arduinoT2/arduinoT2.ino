0ac0548e57a90b65bf75fc0d8ab05d3a326cefb0
Tony Montes <t.montes@uniandes.edu.co>
2023-03-13 17:03:15 -0500

    update arduino motorLR

--- arduinoT2/arduinoT2.ino
+++ arduinoT2/arduinoT2.ino
@@ -1,8 +1,22 @@
 
+int mR1 = 2;
+int mR2 = 3;
+int mL1 = 4;
+int mL2 = 5;
+int pwmR = 6;
+int pwmL = 7;
+
+
 void setup() {
   Serial.begin(9600);
   Serial.setTimeout(1);
 
+  pinMode(mR1, OUTPUT);
+  pinMode(mR2, OUTPUT);
+  pinMode(mL1, OUTPUT);
+  pinMode(mL2, OUTPUT);
+  pinMode(pwmR, OUTPUT);
+  pinMode(pwmL, OUTPUT);
 }
 
 void loop() {
@@ -17,6 +31,14 @@ void loop() {
       Serial.print("y = ");
       Serial.print(y);
 
-      // if x > 0 ...
-    }
+      if (x > 0) {
+        digitalWrite(mL1, HIGH);
+        digitalWrite(mL2, LOW);
+        digitalWrite(pwmL, 70);
+
+        digitalWrite(mR1, HIGH);
+        digitalWrite(mR2, LOW);
+        digitalWrite(pwmR, 70);
+      }
+   }
 }
