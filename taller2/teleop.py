f2fdfc426fcbfb3596d421b62238eff0a2ba6479
sguerrero3 <s.guerrero3@uniandes.edu.co>
2023-03-13 18:22:03 -0500

    holi

--- taller2/teleop.py
+++ taller2/teleop.py
@@ -14,7 +14,7 @@ class TurtleBotTeleop(Node):
     def __init__(self):
         super().__init__('turtle_bot_teleop')
         self.publisher_ = self.create_publisher(Twist, 'turtlebot_cmdVel', 20)
-        self.joystick = pygame.joystick.get_count()
+        self.joystick = pygame.joystick.Joystick(0)
         self.joystick.init()
         self.timer = self.create_timer(0.1, self.timer_callback)
 
@@ -60,48 +60,10 @@ class TurtleBotTeleop(Node):
             print(mov)
 
 
-    def on_presss(self, key):
-
-
-
-        if not(hasattr(key, 'char')): 
-            self.get_logger().info('Invalid input')
-            return # evitar que muera la terminal
-        
-        msg = Twist()
-        if key.char == 'w':
-            msg.linear.x=self.linear
-            self.get_logger().info('Publishing: Adelante')
-        elif key.char == 's':
-            msg.linear.x=-self.linear
-            self.get_logger().info('Publishing: Atras')
-        elif key.char == 'a':
-            msg.angular.z=self.angular
-            self.get_logger().info('Publishing: Izquierda')
-        elif key.char == 'd':
-            msg.angular.z=-self.angular
-            self.get_logger().info('Publishing: Derecha')
-        else:
-            self.get_logger().info(f'Invalid input')
-            return
-
-        self.publisher_.publish(msg)
-
-    def on_release(self, key):
-        if not(hasattr(key, 'char')) or key.char not in ['w', 's', 'a', 'd']:
-            self.get_logger().info(f'Invalid input')
-            return
-        msg = Twist()
-        msg.linear.x=0.0
-        msg.angular.z=0.0
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: Stop')
-        self.prevchar = ''
-
-
 def main(args=None):
     rclpy.init(args=args)
     
+    pygame.init()
     teleop = TurtleBotTeleop()
 
     rclpy.spin(teleop)
