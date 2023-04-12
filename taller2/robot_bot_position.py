e67703b5f70fb4f1895983a78f46ff2319eb57a3
t-montes <t.montes@uniandes.edu.co>
2023-04-12 17:25:09 -0500

    Add Odometry

--- taller2/robot_bot_position.py
+++ taller2/robot_bot_position.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import rclpy
 import time
 from rclpy.node import Node
-
+from math import pi, cos, sin, atan2
 from std_msgs.msg import String
 
 
@@ -15,24 +15,60 @@ class PositionPublisher(Node):
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
+        self.update_freq = timer_period # s
+
+        #RUEDAS # TODO: Cambiar a los valores reales
+        self.r1_radio = 0.05 # m
+        self.r1_resolucion = 640 # ticks/rev
+
+        self.r2_radio = 0.05 # m
+        self.r2_resolucion = 640 # ticks/rev
+
+        self.distancia_ruedas = 10 # m
+
+
         #ENCODER
         self.pin_enc1_a = 18
         self.pin_enc1_b = 23
+
+        self.pin_enc2_a = ... # TODO
+        self.pin_enc2_b = ... # TODO
+
         GPIO.setup(self.pin_enc1_a, GPIO.IN)
         GPIO.setup(self.pin_enc1_b, GPIO.IN)
+        
+        #GPIO.setup(self.pin_enc1_a, GPIO.IN)
+        #GPIO.setup(self.pin_enc1_b, GPIO.IN)
 
         self.count_enc1 = 0
+        self.count_enc2 = 0
         self.current_time = 0
         self.previous_time = 0
-        self.resolucion = 640
-        self.direccion = True
+        self.direccion1 = True
+        #self.direccion2 = True
 
         GPIO.add_event_detect(self.pin_enc1_a, GPIO.RISING, callback=self.encoder1_callback)
+        #GPIO.add_event_detect(self.pin_enc2_a, GPIO.RISING, callback=self.encoder2_callback)
 
+        self.x = 0
+        self.y = 0
+        self.theta = 0
 
     def timer_callback(self):        
+        w1, w2 = self.getVelocidad() # Velocidad rueda izq (w1) y rueda derecha (w2)
+        
+        # ODOMETRIA
+        v1 = w1 * self.r1_radio
+        v2 = w2 * self.r2_radio
 
-        vel = self.getVelocidad()
+        v = (v1 + v2)/2 # Velocidad lineal (m/s)
+        w = (v2 - v1)/self.distancia_ruedas # Velocidad angular (rad/s)
+
+        dt = self.update_freq # Tiempo de actualizacion (s)
+        self.x += v * cos(self.theta) * dt # Distancia recorrida en x (en m)
+        self.y += v * sin(self.theta) * dt # Distancia recorrida en y (en m)
+        self.theta += w * dt # Angulo recorrido (en rad)
+        self.theta = atan2(sin(self.theta), cos(self.theta)) # Normalización del angulo
 
         msg = "Encoder 1 count: {}".format(self.vel)
 
@@ -40,29 +76,37 @@ class PositionPublisher(Node):
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
 
-
     def getVelocidad(self):
-
         self.current_time = time.time()
 
-        if (self.current_time - self.previous_time > 1000):
-            self.previous_time = self.current_time
-            rpm = self.count_enc1*60/self.resolucion
-            self.count_enc1 = 0
-        return rpm
+        self.previous_time = self.current_time
+        rpm1 = self.count_enc1*60/self.r1_resolucion
+        rpm2 = self.count_enc2*60/self.r2_resolucion
+        self.count_enc1 = 0
+        self.count_enc2 = 0
+        
+        return (rpm1, rpm2)
 
 
     def encoder1_callback(self, pin):
-    
         val = GPIO.input(self.pin_enc1_b)
 
         if (val == GPIO.LOW):
-            self.direccion = False
+            self.direccion1 = False
             self.count_enc1 += 1
         else:
-            self.direccion = True
+            self.direccion1 = True
             self.count_enc1 -= 1
 
+    def encoder2_callback(self, pin):
+        val = GPIO.input(self.pin_enc2_b)
+
+        if (val == GPIO.LOW):
+            self.direccion2 = False
+            self.count_enc2 += 1
+        else:
+            self.direccion2 = True
+            self.count_enc2 -= 1
         
 
 def main(args=None):
